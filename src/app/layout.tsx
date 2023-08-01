16091c5040f7bfbfabb1532657a4f193a3025138
Piyush Assudani <63794264+assudani66@users.noreply.github.com>
2023-08-01 15:39:51 +0530

    removeFont error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 "use client"
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { QueryClient, QueryClientProvider } from 'react-query'
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
